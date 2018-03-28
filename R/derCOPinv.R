475e6dfc0b2d68c8653d2f44dd4babad9f04ec90
wasquith <wasquith@BagPipe.local>
2018-03-28 06:51:13 -0500

    enhanced the error trapping

--- R/derCOPinv.R
+++ R/derCOPinv.R
@@ -5,6 +5,13 @@ function(cop=NULL, u, t,
     func <- function(x,u,LHS,cop,delu=delu,para=para, ...) {
             LHS - derCOP(cop=cop, u=u, v=x, delu=delu, para=para, ...)
     }
+    f.lower <- func(0,u,t,cop,delu=delu,para=para, ...)
+    f.upper <- func(1,u,t,cop,delu=delu,para=para, ...)
+    if(sign(f.lower) != sign(f.upper)) {
+      warning("not opposite signs for f.lower and f.upper: u=",u, "and t=",t)
+      return(NA)
+    }
+
     my.rt <- NULL
     try(my.rt <- uniroot(func,interval=c(0,1), u=u, LHS=t,
                               cop=cop, delu=delu, para=para, ...))
