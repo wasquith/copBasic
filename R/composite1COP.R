5e8b1d0fe8dcd93e684cfa3a56e0300936a3e98f
wasquith <wasquith@lochlamentation.attlocal.net>
2018-04-06 21:33:50 -0500

    fixed note and passing of ... to COP()

--- R/composite1COP.R
+++ R/composite1COP.R
@@ -3,6 +3,6 @@ function(u,v,para,...) {
   alpha <- para$alpha; alpha.p <- 1 - alpha
   beta  <- para$beta;   beta.p <- 1 - beta
   return(u^alpha * v^beta *
-         COP(cop=para$cop1, u^alpha.p, v^beta.p, para=para$para1))
+         COP(cop=para$cop1, u^alpha.p, v^beta.p, para=para$para1, ...))
 }
 
--- R/composite2COP.R
+++ R/composite2COP.R
@@ -2,7 +2,7 @@
 function(u,v,para,...) {
   alpha <- para$alpha; alpha.p <- 1 - alpha
   beta  <- para$beta;   beta.p <- 1 - beta
-  return(COP(cop=para$cop1, u^alpha,   v^beta,   para=para$para1) *
-         COP(cop=para$cop2, u^alpha.p, v^beta.p, para=para$para2))
+  return(COP(cop=para$cop1, u^alpha,   v^beta,   para=para$para1, ...) *
+         COP(cop=para$cop2, u^alpha.p, v^beta.p, para=para$para2, ...))
 }
 
