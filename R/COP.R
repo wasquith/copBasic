7e10841bb34b9a95fae405c2e3fe7e640ac1575d
wasquith <william.asquith@ttu.edu>
2018-04-21 09:11:03 -0500

    added support for 1,2,3,4 on the reflection and removed the vectorization as the dispatch to cop() will have to do that anyway

--- R/COP.R
+++ R/COP.R
@@ -1,20 +1,22 @@
 "COP" <-
-function(u, v, cop=NULL, para=NULL, reflect=c("cop", "surv", "acute", "grave"), ...) {
+function(u, v, cop=NULL, para=NULL,
+               reflect=c("cop", "surv", "acute", "grave",
+                           "1",    "2",     "3",     "4"), ...) {
    reflect <- match.arg(reflect)
-   if(length(u) > 1 & length(v) > 1 & length(u) != length(v)) {
-      warning("length u = ", length(u), " and length v = ", length(v))
-      warning("longer object length is not a multiple of shorter object length, no recycling")
-      return(NA)
+   if(is.list(para)) {
+      reflect   <- para$reflect
+      if(is.null(reflect)) reflect <- "cop"
+      cop  <- para$cop
+      para <- para$para
    }
-   if(length(u) == 1) {
-      u <- rep(u, length(v))
-   } else if(length(v) == 1) {
-      v <- rep(v, length(u))
-   }
-   cop <- switch(reflect,
+   return(switch(reflect,
                  cop   =             cop(u,     v, para=para, ...),
                  surv  = u + v - 1 + cop(1-u, 1-v, para=para, ...),
                  acute =     v     - cop(1-u,   v, para=para, ...),
-                 grave = u         - cop(  u, 1-v, para=para, ...) )
-  return(cop)
+                 grave = u         - cop(  u, 1-v, para=para, ...),
+                 "1"   =             cop(u,     v, para=para, ...),
+                 "2"   = u + v - 1 + cop(1-u, 1-v, para=para, ...),
+                 "3"   =     v     - cop(1-u,   v, para=para, ...),
+                 "4"   = u         - cop(  u, 1-v, para=para, ...)
+         ))
 }
