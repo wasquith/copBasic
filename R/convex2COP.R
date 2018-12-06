d7b7c50a701ef5918880426335e91d273535f0e8
wasquith <william.asquith@ttu.edu>
2018-12-06 07:30:00 -0600

    check on missing alpha

--- R/convex2COP.R
+++ R/convex2COP.R
@@ -1,5 +1,9 @@
 "convex2COP" <-
 function(u,v, para, ...) {
+  if(is.null(para$alpha)) {
+    warning("no alpha parameter given")
+    return(NULL)
+  }
   return(   para$alpha  * COP(u, v, cop=para$cop1, para=para$para1) +
          (1-para$alpha) * COP(u, v, cop=para$cop2, para=para$para2))
 }
