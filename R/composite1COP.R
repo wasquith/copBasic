312115eb9800fe6d9b317c8330f33d1363a7ae46
wasquith <william.asquith@ttu.edu>
2018-12-06 07:26:35 -0600

    added warnings about missing alpha and beta

--- R/composite1COP.R
+++ R/composite1COP.R
@@ -1,5 +1,13 @@
 "composite1COP" <-
 function(u,v,para,...) {
+  if(is.null(para$alpha)) {
+    warning("no alpha parameter given")
+    return(NULL)
+  }
+  if(is.null(para$beta)) {
+    warning("no beta parameter given")
+    return(NULL)
+  }
   alpha <- para$alpha; alpha.p <- 1 - alpha
   beta  <- para$beta;   beta.p <- 1 - beta
   return(u^alpha * v^beta *
