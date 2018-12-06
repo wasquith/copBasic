5cdcc58888bcb7d99ab158c5aea406efc051e41c
wasquith <william.asquith@ttu.edu>
2018-12-06 07:29:09 -0600

    added warning on missing kappa and gamma

--- R/composite3COP.R
+++ R/composite3COP.R
@@ -1,5 +1,13 @@
 "composite3COP" <-
 function(u,v,para,...) {
+  if(is.null(para$kappa)) {
+    warning("no kappa parameter given")
+    return(NULL)
+  }
+  if(is.null(para$gamma)) {
+    warning("no gamma parameter given")
+    return(NULL)
+  }
   kappa <- para$kappa; kappa.p <- 1 - kappa
   gamma <- para$gamma; gamma.p <- 1 - gamma
   return(u^kappa * v^gamma * composite2COP(u^kappa.p, v^gamma.p, para, ...))
