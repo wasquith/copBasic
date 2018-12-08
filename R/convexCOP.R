aff8ae0a5e2a34d0132f9e8755bc8b754bf18b92
wasquith <william.asquith@ttu.edu>
2018-12-08 08:40:29 -0600

    vectorization fix

--- R/convexCOP.R
+++ R/convexCOP.R
@@ -14,14 +14,28 @@
      weights <- get("weights", para)
   }
 
-  weights <-  weights/sum(weights)
+  weights   <- weights/sum(weights)
+  cop.names <- nm[cops.idx]; para.names <- nm[pars.idx]
 
-  cop.names  <- nm[cops.idx]; para.names <- nm[pars.idx]
+  if(length(u) > 1 & length(v) > 1 & length(u) != length(v)) {
+   warning("length u = ", length(u), " and length v = ", 
+           length(v))
+   warning("longer object length is not a multiple of shorter object length, ", 
+           "no recycling")
+   return(NA)
+  }
+  if(length(u) == 1) {
+    u <- rep(u, length(v))
+  } else if (length(v) == 1) {
+    v <- rep(v, length(u))
+  }
 
-  GG <- sapply(1:n, function(i) {
+  HH <- sapply(1:length(u), function(k) {
+          gg <- sapply(1:n, function(i) {
                     the.cop  <- get(cop.names[i],  para)
                     the.para <- get(para.names[i], para)
-                    weights[i]*COP(u,v, cop=the.cop, para=the.para) })
-  return(sum(GG))
+                    weights[i]*the.cop(u[k],v[k], para=the.para) })
+          return(sum(gg)) })
+  return(HH)
 }
 
