9043f6bd3e818d77b53525c44e83abd7f53fd2d7
wasquith <william.asquith@ttu.edu>
2019-01-01 14:14:50 -0600

    parelleled reflect options with COP

--- R/simCOPmicro.R
+++ R/simCOPmicro.R
@@ -1,21 +1,26 @@
 "simCOPv" <-
 function(u, cop=NULL, para=NULL,
-            reflect=c("cop", "surv", "acute", "grave"), ...) {
+            reflect=c("cop", "surv", "acute", "grave",
+                        "1",    "2",     "3",     "4"), ...) {
    simCOPmicro(u, cop=cop, para=para, reflect=reflect, ...)
 }
 
 
 "simCOPmicro" <-
 function(u, cop=NULL, para=NULL,
-            reflect=c("cop", "surv", "acute", "grave"), ...) {
+            reflect=c("cop", "surv", "acute", "grave",
+                        "1",    "2",     "3",     "4"), ...) {
   reflect <- match.arg(reflect)
   n <- length(u); t <- runif(n); v <- vector(mode="numeric", length=n)
   v <- switch(reflect,
     cop   = sapply(1:n, function(i) {     derCOPinv(cop=cop,   u[i],   t[i], para=para, ...) }),
     surv  = sapply(1:n, function(i) { 1 - derCOPinv(cop=cop, 1-u[i], 1-t[i], para=para, ...) }),
     acute = sapply(1:n, function(i) {     derCOPinv(cop=cop, 1-u[i],   t[i], para=para, ...) }),
-    grave = sapply(1:n, function(i) { 1 - derCOPinv(cop=cop,   u[i], 1-t[i], para=para, ...) })
-  )
+    grave = sapply(1:n, function(i) { 1 - derCOPinv(cop=cop,   u[i], 1-t[i], para=para, ...) }),
+    "1"   = sapply(1:n, function(i) {     derCOPinv(cop=cop,   u[i],   t[i], para=para, ...) }),
+    "2"   = sapply(1:n, function(i) { 1 - derCOPinv(cop=cop, 1-u[i], 1-t[i], para=para, ...) }),
+    "3"   = sapply(1:n, function(i) {     derCOPinv(cop=cop, 1-u[i],   t[i], para=para, ...) }),
+    "4"   = sapply(1:n, function(i) { 1 - derCOPinv(cop=cop,   u[i], 1-t[i], para=para, ...) }))
   if(any(is.na(v))) warning("could not uniroot at least for one element in derCOPinv")
   return(v)
 }
