23c7bbbd51be496f5a6e23de5491c5a51ebc60a4
wasquith-usgs <wasquith@usgs.gov>
2020-03-17 11:40:42 -0500

    initial commit

---
+++ srcall.R
@@ -0,0 +1,6 @@
+# Change to the directory containing the top-level of the package
+# and then source this file to source all of the R files.
+# This utility is stricly for the core developer to test things
+# without ever having to actually install the package!
+for(f in list.files("./R",pattern="\\.R")) source(paste0("./R/",f))
+
