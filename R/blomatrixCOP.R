5d207e4598f64eff2e081032a1f846f7d4a0dca3
wasquith <william.asquith@ttu.edu>
2021-11-17 19:11:54 -0600

    finally correct implementation

--- R/blomatrixCOP.R
+++ R/blomatrixCOP.R
@@ -1,5 +1,12 @@
 blomatrixCOPdec <-
-function(cop=NULL, para=NULL, as.sample=FALSE, ...) {
+function(cop=NULL, para=NULL, as.sample=FALSE,
+                  ctype=c("na", "weibull", "hazen",
+                                "1/n", "bernstein", "checkerboard"), ...) {
+  ctype <- match.arg(ctype)
+  if(ctype != "na") { # a hack to keep similar to blomCOP
+    as.sample <- TRUE
+  }
+  t <- c(0.10, 0.50, 0.90)
   if(as.sample) {
     if(is.null(para)) {
       warning("Sample Blomqvist's Beta desired but para is NULL, returning NULL")
@@ -9,16 +16,20 @@ function(cop=NULL, para=NULL, as.sample=FALSE, ...) {
       warning("para argument must be data.frame having only two columns, returning NULL")
       return(NULL)
     }
-    u <- para[,1]; v <- para[,2]; n <- length(u)
-    #A <- (1+n)/2
-    #samBLOM <- (2/n)*(sum(as.numeric((rank(u) - A)*(rank(v) - A) >= 0))) - 1
-    return("not yet implemented")
+    A <- 1 / P(t, t[3])
+    B <- 1 / P(t, t[2])
+    C <- 1 / P(t, t[1])
+    blom <- matrix(c(EMPIRcop(t, t[3], para=para, ctype=ctype, ...) * A - 1,
+                     EMPIRcop(t, t[2], para=para, ctype=ctype, ...) * B - 1,
+                     EMPIRcop(t, t[1], para=para, ctype=ctype, ...) * C - 1), ncol=3)
+    colnames(blom) <- paste0("U|V=", c("0.10", "0.50", "0.90"))
+    rownames(blom) <- rev(colnames(blom))
+    return(blom)
   } else {
     if(is.null(cop)) {
       warning("must have copula argument specified, returning NULL")
       return(NULL)
     }
-    t <- c(0.10, 0.50, 0.90)
     A <- 1 / P(t, t[3])
     B <- 1 / P(t, t[2])
     C <- 1 / P(t, t[1])
@@ -33,7 +44,13 @@ function(cop=NULL, para=NULL, as.sample=FALSE, ...) {
 
 
 blomatrixCOPiqr <-
-function(cop=NULL, para=NULL, as.sample=FALSE, ...) {
+function(cop=NULL, para=NULL, as.sample=FALSE,
+                  ctype=c("na", "weibull", "hazen",
+                                "1/n", "bernstein", "checkerboard"), ...) {
+  ctype <- match.arg(ctype)
+  if(ctype != "na") { # a hack to keep similar to blomCOP
+    as.sample <- TRUE
+  }
   t <- c(0.25, 0.50, 0.75)
   if(as.sample) {
     if(is.null(para)) {
@@ -44,10 +61,18 @@ function(cop=NULL, para=NULL, as.sample=FALSE, ...) {
       warning("para argument must be data.frame having only two columns, returning NULL")
       return(NULL)
     }
-    samBLOM <- matrix(EMPIRcop(t, t[3], para=para, ...),
-                      EMPIRcop(t, t[2], para=para, ...),
-                      EMPIRcop(t, t[1], para=para, ...), ncol=3)
-    return(samBLOM)
+    #A <- c(16/3, 16/6, 16/9)
+    #B <- c(16/2, 16/4, 16/6)
+    #C <- c(16/1, 16/2, 16/3)
+    A <- 1 / P(t, t[3])
+    B <- 1 / P(t, t[2])
+    C <- 1 / P(t, t[1])
+    blom <- matrix(c(EMPIRcop(t, t[3], para=para, ctype=ctype, ...) * A - 1,
+                     EMPIRcop(t, t[2], para=para, ctype=ctype, ...) * B - 1,
+                     EMPIRcop(t, t[1], para=para, ctype=ctype, ...) * C - 1), ncol=3)
+    colnames(blom) <- paste0("U|V=", c("0.25", "0.50", "0.75"))
+    rownames(blom) <- rev(colnames(blom))
+    return(blom)
   } else {
     if(is.null(cop)) {
       warning("must have copula argument specified, returning NULL")
