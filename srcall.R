d483f98d84535f9056f1fa5f2dcf0db64ac9a351
wasquith <william.asquith@ttu.edu>
2023-06-10 09:49:02 -0500

    auto directory change added

--- srcall.R
+++ srcall.R
@@ -1,6 +1,7 @@
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
 # Change to the directory containing the top-level of the package
 # and then source this file to source all of the R files.
-# This utility is stricly for the core developer to test things
+# This utility is strictly for the core developer to test things
 # without ever having to actually install the package!
-for(f in list.files("./R",pattern="\\.R")) source(paste0("./R/",f))
-
+for(f in list.files("./R", pattern="\\.R")) source(paste0("./R/", f))
