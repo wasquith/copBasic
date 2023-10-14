adb18184850b628404444eb6fe016b36c2a4e898
wasquith <william.asquith@ttu.edu>
2023-10-14 14:29:30 -0500

    added support for u and v or two columns in u

--- R/bicCOP.R
+++ R/bicCOP.R
@@ -1,10 +1,14 @@
 "bicCOP" <-
-function(u,v, cop=NULL, para=NULL, m=NA, ...) {
+function(u, v=NULL, cop=NULL, para=NULL, m=NA, ...) {
+  if(is.null(v)) {
+    v <- u[,2]
+    u <- u[,1]
+  }
   if(is.null(cop)) {
     warning("must have copula argument specified, returning NULL")
     return(NULL)
   }
-  theo.cop <-      cop(u,v, para=para,            ...)
+  theo.cop <- COP(u, v, cop=cop, para=para, ...)
   emp.cop  <- EMPIRcop(u,v, para=data.frame(u,v), ...)
   n <- length(theo.cop); k <- length(emp.cop)
   if(n != k) return(Inf)
--- R/statTn.R
+++ R/statTn.R
@@ -1,11 +1,15 @@
 "statTn" <-
- function(uv, cop=NULL, para=NULL, p=2, proot=FALSE, ...) {
+ function(u, v=NULL, cop=NULL, para=NULL, p=2, proot=FALSE, ...) {
+   if(is.null(v)) {
+     v <- u[,2]
+     u <- u[,1]
+   }
    proot  <- ifelse(proot, 1/p, 1)
-   parcop <- COP(uv[,1], uv[,2], cop=cop, para=para, ...)
+   parcop <- COP(u, v, cop=cop, para=para, ...)
    # The empirical is called 2nd because it can be CPU intensive
-   # and if the parameteric call is going to error, let us have it
+   # and if the parametric call is going to error, let us have it
    # do it first and not waste users time.
-   empcop <- EMPIRcopdf(para=uv, ...)$empcop
+   empcop <- EMPIRcopdf(para=data.frame(u,v), ...)$empcop
    (sum((parcop - empcop)^p))^proot
 }
 
