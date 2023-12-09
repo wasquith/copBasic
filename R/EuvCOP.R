26a128f597c84bcd0c78a1063f7a3e5a63569dd7
wasquith <william.asquith@ttu.edu>
2023-12-09 10:53:13 -0600

    add MC integration if numerical fails

--- R/EuvCOP.R
+++ R/EuvCOP.R
@@ -1,13 +1,16 @@
 "EuvCOP" <-
-function(v=seq(0.01, 0.99, by=0.01), cop=NULL, para=NULL, asuv=FALSE,
+function(v=seq(0.01, 0.99, by=0.01), cop=NULL, para=NULL, asuv=FALSE, nsim=1E5,
          subdivisions=100L, rel.tol=.Machine$double.eps^0.25, abs.tol=rel.tol, ...) {
 
    u <- sapply(v, function(t) {
           E <- NULL
-          try(E <- integrate(function(k) { 1-derCOP2(cop=cop, para=para, k, t, ...) },
+          try(E <- integrate(function(k) { 1 - derCOP2(cop=cop, para=para, k, t, ...) },
                     lower=0, upper=1, subdivisions=subdivisions,
                     rel.tol=rel.tol, abs.tol=abs.tol)$value, silent=TRUE)
-          if(is.null(E)) return(NA)
+          if(is.null(E)) {
+            E <- mean( 1 - derCOP2(cop=cop, para=para, runif(nsim), t, ...) )
+            names(E) <- "sim"
+          }
           return(E)
         })
    u[u <= 0] <- 0
--- R/EvuCOP.R
+++ R/EvuCOP.R
@@ -1,13 +1,16 @@
 "EvuCOP" <-
-function(u=seq(0.01, 0.99, by=0.01), cop=NULL, para=NULL, asuv=FALSE,
+function(u=seq(0.01, 0.99, by=0.01), cop=NULL, para=NULL, asuv=FALSE, nsim=1E5,
          subdivisions=100L, rel.tol=.Machine$double.eps^0.25, abs.tol=rel.tol, ...) {
 
    v <- sapply(u, function(t) {
           E <- NULL
-          try(E <- integrate(function(k) { 1-derCOP( cop=cop, para=para, t, k, ...) },
+          try(E <- integrate(function(k) { 1 - derCOP( cop=cop, para=para, t, k, ...) },
                     lower=0, upper=1, subdivisions=subdivisions,
                     rel.tol=rel.tol, abs.tol=abs.tol)$value, silent=TRUE)
-          if(is.null(E)) return(NA)
+          if(is.null(E)) {
+            E <- mean( 1 - derCOP(cop=cop, para=para, t, runif(nsim), ...) )
+            names(E) <- "sim"
+          }
           return(E)
         })
    v[v <= 0] <- 0
