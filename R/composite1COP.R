e89a1f808d40e0897fa506b9dc6bc9dbf30fb3bd
wasquith <william.asquith@ttu.edu>
2024-12-23 16:40:09 -0600

    upgraded with reference to khoudraji device

--- R/composite1COP.R
+++ R/composite1COP.R
@@ -17,3 +17,41 @@ function(u, v, para, ...) {
   return(u^alpha * v^beta * COP(u^alpha.p, v^beta.p, para=para, ...))
 }
 
+"khoudraji1COP" <-
+function(u, v, para, ...) {
+  if(is.null(para$alpha)) {
+    warning("no alpha parameter given")
+    return(NULL)
+  }
+  if(is.null(para$beta)) {
+    warning("no beta parameter given")
+    return(NULL)
+  }
+  alpha <- para$alpha; alpha.p <- 1 - alpha
+  beta  <- para$beta;   beta.p <- 1 - beta
+
+  if(is.null(para$para)) para$para <- para$para1
+  if(is.null(para$cop )) para$cop  <- para$cop1
+
+  return(u^alpha * v^beta * COP(u^alpha.p, v^beta.p, para=para, ...))
+}
+
+
+"khoudrajiPCOP" <-
+function(u, v, para, ...) {
+  if(is.null(para$alpha)) {
+    warning("no alpha parameter given")
+    return(NULL)
+  }
+  if(is.null(para$beta)) {
+    warning("no beta parameter given")
+    return(NULL)
+  }
+  alpha <- para$alpha; alpha.p <- 1 - alpha
+  beta  <- para$beta;   beta.p <- 1 - beta
+
+  if(is.null(para$para)) para$para <- para$para1
+  if(is.null(para$cop )) para$cop  <- para$cop1
+
+  return(u^alpha * v^beta * COP(u^alpha.p, v^beta.p, para=para, ...))
+}
--- R/composite2COP.R
+++ R/composite2COP.R
@@ -1,5 +1,5 @@
 "composite2COP" <-
-function(u,v,para,...) {
+function(u, v, para, ...) {
   if(is.null(para$alpha)) {
     warning("no alpha parameter given")
     return(NULL)
@@ -14,3 +14,18 @@ function(u,v,para,...) {
          COP(cop=para$cop2, u^alpha.p, v^beta.p, para=para$para2, ...))
 }
 
+"khoudraji2COP" <-
+function(u, v, para, ...) {
+  if(is.null(para$alpha)) {
+    warning("no alpha parameter given")
+    return(NULL)
+  }
+  if(is.null(para$beta)) {
+    warning("no beta parameter given")
+    return(NULL)
+  }
+  alpha <- para$alpha; alpha.p <- 1 - alpha
+  beta  <- para$beta;   beta.p <- 1 - beta
+  return(COP(cop=para$cop1, u^alpha,   v^beta,   para=para$para1, ...) *
+         COP(cop=para$cop2, u^alpha.p, v^beta.p, para=para$para2, ...))
+}
