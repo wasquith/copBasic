1815ab14c091851f555780964b74c63a6aa71f45
wasquith <william.asquith@ttu.edu>
2025-08-14 17:37:30 -0500

    code v-alignment

--- R/rmseCOP.R
+++ R/rmseCOP.R
@@ -8,8 +8,8 @@ function(u, v=NULL, cop=NULL, para=NULL, ...) {
     warning("must have copula argument specified, returning NULL")
     return(NULL)
   }
-  theo.cop <- COP(u, v, cop=cop, para=para, ...)
-  emp.cop  <- EMPIRcop(u,v, para=data.frame(u,v), ...)
+  theo.cop <-      COP(u, v, cop=cop, para=para,            ...)
+  emp.cop  <- EMPIRcop(u, v,          para=data.frame(u,v), ...)
   n <- length(theo.cop); k<- length(emp.cop)
   if(n != k) return(Inf)
   mse <- mean((theo.cop - emp.cop)^2)
