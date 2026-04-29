a4f137cb110798441e1ef7bc6089983f7465940a
wasquith <33225874+wasquith@users.noreply.github.com>
2026-04-29 17:11:56 -0500

    operational but subject to more checks

--- R/wolfCOPtest.R
+++ R/wolfCOPtest.R
@@ -1,4 +1,4 @@
-"wolfCOPtest" <-
+"wolfCOPtest2" <-
 function(x, y, asuv=FALSE, aslist=TRUE, na.rm=TRUE, digits=6,
                probs=c(0.90, 0.95, 0.98, 0.99, 0.995), ...) {
   # The probs are quantile levels of the sigma to report, and these are useful to check against the
@@ -9,10 +9,10 @@ function(x, y, asuv=FALSE, aslist=TRUE, na.rm=TRUE, digits=6,
   if( length(probs) == "") probs <- 0.95 # 95th percentile or rather the 5-percent critical value (upper tail).
 
   if(length(x) == 1) { # If x is just one value, then it is treated as the Schweizer-Wolff Sigma
-    rwolf <- x[1]; lwolf <- log(rwolf/(1-rwolf)); n <- ngiven <- y[1] # and the sample size is in y[1]
+    rwolf <- x[1]; lwolf <- log(rwolf/(1-rwolf)); n <- y[1] # and the sample size is in y[1]
     if(n < 3) {
-      warning("sample size is <3; so, will treat it as if it were 3 and preserve original sample by negation")
-      ngiven <- -n; n <- 3
+      warning("sample size is <3, returning NULL")
+      return(NULL)
     }
   } else {
     if(length(x) != length(y)) {
@@ -21,10 +21,13 @@ function(x, y, asuv=FALSE, aslist=TRUE, na.rm=TRUE, digits=6,
     }
     uv <- data.frame(u=x, v=y)
     if(na.rm) uv <- uv[complete.cases(uv),]
-    n <- ngiven <- nrow(uv) # sample size
-    if(n < 3) {
-      warning("sample size is <3; so, will treat it as if it were 9 and preserve original sample by negation")
-      ngiven <- -n; n <- 3
+    n <- nrow(uv) # sample size
+    if(n < 3) { # This handling of the sample size dates from an much earlier version of this function
+      # that had a lower limit of 9. With the empirical distributions for sample sizes 3-40 now supported,
+      # we drop the minimum sample size down to 3 but with the logic here, we effectively permit samples
+      # sizes to be incoming down to
+      warning("sample size is <3; returning NULL")
+      return(NULL)
     }
     if(! asuv) { # if true, then the user has provided the paired observations of probability
       uv[,1] <- lmomco::pp(uv[,1], sort=FALSE, ...)
@@ -47,10 +50,10 @@ function(x, y, asuv=FALSE, aslist=TRUE, na.rm=TRUE, digits=6,
     # Nonlinear regression coefficients computed PRESS minimization of residuals for the
     # exponent on log10(sample size) term. The regressions come from simulation of the Sigma
     # distribution (its logit) assuming the Independence copula.
-    mucoe <- c(-0.00495326, -1.09756265, 1.06987736, -1.30781258)
-    l2coe <- c(0.13767621, -0.00299843, 0.08801107, -2.229492198)
-    t3coe <- c(0.07968628, 0.00539535, 0.16098291, -1.916406258)
-    t4coe <- c(0.12410138, -0.00012268, 0.0437761, -2.7531258)
+    mucoe <- c(0.04564608, -1.10684982, 1.02858995, -1.36093758)
+    l2coe <- c(0.12772039, -0.00036535, 0.0954984, -2.036914068)
+    t3coe <- c(0.08827982, 0.0032132, 0.15446976, -2.001953138)
+    t4coe <- c(0.12558378, -0.00054097, 0.04265488, -2.856258)
   # DISABLED } else {
   # DISABLED   # Nonlinear regression coefficients computed PRESS minimization of residuals for the
   # DISABLED   # exponent on log10(sample size) term. The regressions come from simulation of the Sigma
@@ -62,10 +65,16 @@ function(x, y, asuv=FALSE, aslist=TRUE, na.rm=TRUE, digits=6,
   # DISABLED }
 
   # Apply the regressions using the hardwired coefficients herein
+  # m <- 3000 # sample sizes for which we declare that Tau3 and Tau4 have become constant, which is
+  # is technically close to reality but with the curvilinear regression being used, we eschew the
+  # prediction not being monotonic decreasing with sample size want it to have an apparent asymptote.
+  m <- ifelse(n > 100000, 100000, n) # This keeps the apparent trajectory of a Tau3 and Tau4 plot
+  # having a hook in it as sample sizes increases to infinite.
   mu    <- mucoe[1] + mucoe[2] * log10(n) + mucoe[3] * log10(n)^mucoe[4] # Mean    (Lambda1)
   l2    <- l2coe[1] + l2coe[2] * log10(n) + l2coe[3] * log10(n)^l2coe[4] # Lambda2 (L-scale)
-  t3    <- t3coe[1] + t3coe[2] * log10(n) + t3coe[3] * log10(n)^t3coe[4] # Tau3    (L-skew)
-  t4    <- t4coe[1] + t4coe[2] * log10(n) + t4coe[3] * log10(n)^t4coe[4] # Tau4    (L-kurtosis)
+  t3    <- t3coe[1] + t3coe[2] * log10(m) + t3coe[3] * log10(m)^t3coe[4] # Tau3    (L-skew)
+  t4    <- t4coe[1] + t4coe[2] * log10(m) + t4coe[3] * log10(m)^t4coe[4] # Tau4    (L-kurtosis)
+  if(t4 < (5 * t3^2 - 1)/4) t4 <- (5 * t3^2 - 1)/4
   lmrs  <- c(mu, l2, t3, t4) # Tidy list of the Lmoments of the logit(Sigma) distribution
   if(dtype == "gno") {
     para  <-  lmomco::pargno(lmomco::vec2lmom(c(mu, l2, t3, t4)), useHosking=FALSE)
@@ -106,34 +115,39 @@ function(x, y, asuv=FALSE, aslist=TRUE, na.rm=TRUE, digits=6,
   # We appear to use less system time on system.time(), when we find the file and load it instead
   # of using the data() call.
   smlsam <- system.file("data/wolfCOPtest_data_smlsam.RData", package="copBasic")
-  if(n <= 40 & file.exists(smlsam)) {
+  max_n_in_smlsam <- 40 # max sample size within wolfCOPtest_data_smlsam$n (yes hard wired)
+  if(n <= max_n_in_smlsam & file.exists(smlsam)) {
     wolfCOPtest_data_smlsam <- NULL # initialize whether or no so R CMD check --as-cran will pass by visibility
     load(smlsam)
     #data(wolfCOPtest_data_smlsam) # importFrom("utils", "data") # in NAMESPACE required
     sata <- wolfCOPtest_data_smlsam; suppressWarnings( rm(wolfCOPtest_data_smlsam) )
-    sata <- sata[sata$n == n,]
-    sata <- sata[order(sata$probs),]
-    row.names(sata) <- NULL; # print(sata, 16)
-    suppressWarnings( nep_small <- approx(sata$wolfemp, y=sata$probs, xout=rwolf)$y )
-    pval_small <- round(1 - nep_small, digits=16); names(pval_small) <- "p.value(sample_le40)"
+    sata <- sata[sata$n == n,]       # isolate the table to the sample size of interest
+    if(nrow(sata) == 0) {
+      warning("sample size n=", n, " does not exist in wolfCOPtest_data_smlsam, so set pval_small=NA")
+      pval_small <- NA
+      names(pval_small) <- paste0("p.value(sample_le", max_n_in_smlsam, ")")
+    } else {
+      sata <- sata[order(sata$probs),] # should be sorted already but do so again if needing to inspect
+      row.names(sata) <- NULL; # print(sata, 16)
+      suppressWarnings( nep_small <- approx(sata$wolfemp, y=sata$probs, xout=rwolf)$y )
+      pval_small <- round(1 - nep_small, digits=16)
+      names(pval_small) <- paste0("p.value(sample_le", max_n_in_smlsam, ")")
+    }
   } else {
-    pval_small <- NA; names(pval_small) <- "p.value(sample_le40)"
+    pval_small <- NA; names(pval_small) <- paste0("p.value(sample_le", max_n_in_smlsam, ")")
   }
 
   pval <- round(1 - neps, digits=16); names(pval) <- paste0("p.value(dist_", dtype, ")")
   pval <- c(pval, pval_small)
 
-  zz <- c(n, ngiven, rwolf, lwolf, pval, para$para, lmrs, quans)
-  names(zz) <- c("sample_size_in_comps", "sample_size_given", "sigma", "logit_sigma",
-                 names(pval),
-                 names(para$para), names(lmrs), quatxt)
+  zz <- c(n, rwolf, lwolf, pval, para$para, lmrs, quans)
+  names(zz) <- c("sample_size", "sigma", "logit_sigma",
+                 names(pval), names(para$para), names(lmrs), quatxt)
   names(zz) <- gsub("_TEXT_", "logit", names(zz))
   # DISABLED names(zz) <- gsub("_TEXT_", ifelse(bylogit, "logit", "_"), names(zz))
   if(aslist) {
     wz <- c(rwolf, lwolf); names(wz) <- c("sigma", "logit_sigma")
-    zz <- list(sample_size_in_comps=n, sample_size_given=ngiven,
-               statistic=wz, p.value=pval,
-               distpara_by_lmoms=para$para)
+    zz <- list(sample_size=n, statistic=wz, p.value=pval, distpara_by_lmoms=para$para)
     # DISABLED if(bylogit) {
       zz$lmoms_logit_sigma <- lmrs # L-moments of the logit(SIGMAS) distribution
     # DISABLED } else {
