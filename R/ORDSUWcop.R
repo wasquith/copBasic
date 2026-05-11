9a25e22cd789ac8c78ad4844f83624f717b5734b
wasquith <33225874+wasquith@users.noreply.github.com>
2026-05-10 21:21:33 -0500

    better internal traps

--- R/ORDSUWcop.R
+++ R/ORDSUWcop.R
@@ -15,7 +15,7 @@ function(u,v, para=list(cop=M, para=NA, part=c(0,1)), ...) {
   }
 
   if(is.null(para)) {
-    para <- list(cop=M, para=NULL, part=c(0,1))
+    para <- list(cop=copBasic::M, para=NULL, part=c(0,1))
   }
 
   if(! exists("part", para)) {
@@ -39,51 +39,55 @@ function(u,v, para=list(cop=M, para=NA, part=c(0,1)), ...) {
       warning("para$part given as vector of break points but a number is repeated")
       return(NULL)
     }
-    J <- lapply(seq(1, np-1, by=1), function(i) c(x[i], x[i+1]))
+    myJ <- lapply(seq(1, np-1, by=1), function(i) c(x[i], x[i+1]))
   } else {
-    J <- para$part
+    myJ <- para$part
   }
 
-  num_partitions <- length(J); kx <- seq_len(num_partitions)
+  num_partitions <- length(myJ); kx <- seq_len(num_partitions)
   if(exists("para", para) & is.null(para$para)) {
-    Q <- lapply(kx, function(k) NA)
+    myQ <- lapply(kx, function(k) NA)
   } else if(exists("para", para) & length(para$para) == 1 && is.na(para$para)) {
-    Q <- lapply(kx, function(k) NA)
+    myQ <- lapply(kx, function(k) NA)
   } else if(! exists("para", para)) {
-    Q <- lapply(kx, function(k) NA)
+    myQ <- lapply(kx, function(k) NA)
   } else if(is.list(  para$para) & length(para$para) == 1) {
-    Q <- lapply(kx, function(k) para$para[[1]])
+    myQ <- lapply(kx, function(k) para$para[[1]])
   } else if(is.vector(para$para) & length(para$para) == 1) {
-    Q <- lapply(kx, function(k) para$para)
+    myQ <- lapply(kx, function(k) para$para)
   } else {
-    Q <- para$para
+    myQ <- para$para
   }
 
   if(! exists("cop", para)) {
-    C <- lapply(kx, function(k) W)
+    myC <- lapply(kx, function(k) copBasic::W)
   } else if(length(para$cop) == 1) {
     if(is.list(para$cop)) {
-      C <- lapply(kx, function(k) para$cop[[1]])
+      myC <- lapply(kx, function(k) para$cop[[1]])
     } else {
-      C <- lapply(kx, function(k) para$cop)
+      myC <- lapply(kx, function(k) para$cop)
     }
   } else {
-    C <- para$cop
+    myC <- para$cop
   }
 
-  if(length(J) != length(Q) | length(J) != length(C)) {
+  if(length(myJ) != length(myQ) | length(myJ) != length(myC)) {
     warning("malformed para argument (ORDSUWcop): ",
-            paste(c("J", "Q", "C"), c(length(J), length(Q), length(C)), collapse=",", sep="="))
+            paste(c("myJ", "myQ", "myC"), c(length(myJ), length(myQ), length(myC)),
+            collapse=",", sep="="))
+    message("myJ: "); print(str(myJ))
+    message("myQ: "); print(str(myQ))
+    message("myC: "); print(str(myC))
     return(NULL)
   }
 
   zz <- sapply(seq_len(length(u)), function(i) {
            for(k in kx) {
-             a <- J[[k]][1]; b <- J[[k]][2]; ba <- b-a
+             a <- myJ[[k]][1]; b <- myJ[[k]][2]; ba <- b-a
              # if(u[i] < a | u[i] > b | v[i] < a | v[i] > b) next # outside [a,b]^2
              if(u[i] <   a | u[i] >=     b) next
              if(v[i] < 1-b | v[i] >= 1 - a) next
-             return(a+ba*C[[k]]((u[i]-a)/ba, (v[i]-1+b)/ba, para=Q[[k]], ...))
+             return(a+ba*myC[[k]]((u[i]-a)/ba, (v[i]-1+b)/ba, para=myQ[[k]], ...))
            }; return(max(u[i]+v[i]-1, 0)) }) # W(u,v) otherwise
   return(zz)
 }
