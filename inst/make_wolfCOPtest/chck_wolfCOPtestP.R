d7439e643fdec76c36b8df3f388becc7bbab987f
wasquith <33225874+wasquith@users.noreply.github.com>
2026-05-16 20:43:23 -0500

    Update chck_wolfCOPtestP.R

--- inst/make_wolfCOPtest/chck_wolfCOPtestP.R
+++ inst/make_wolfCOPtest/chck_wolfCOPtestP.R
@@ -11,6 +11,7 @@ library(lmomco)
 
 D <- NULL
 D <- read.table("mc_wolfPI/aa_allsimsREF.txt", sep="\t", header=TRUE)
+# Built by make_wolfCOPtestP_A.R script.
 #files <- sort(list.files(pattern="^mc_wolfPI"))
 #for(f in list.files(pattern="^mc_wolfPI")) {
 #  D <- rbind(D, read.table(f, header=TRUE))
@@ -19,7 +20,6 @@ D <- D[order(D$n),]
 D <- D[complete.cases(D),]
 
 h <- aggregate(D, by=list(D$n), length)
-print(paste0(h$Group.1[h$nsim < 3], collapse=","))
 
 Z <- NULL # now compute weighted mean columns
 for(k in sort(unique(D$n))) {
@@ -83,35 +83,6 @@ plot(Z$n, Z$logitlam2, log="x", cex=log10(Z$nsim)-2.9, col=1); #stop()
 plot(Z$n, Z$logittau3, log="x", cex=log10(Z$nsim)-2.9, col=1, ylim=c(0.1,0.35))
 plot(Z$n, Z$logittau4, log="x", cex=log10(Z$nsim)-2.9, col=1, ylim=c(0.1,0.20))
 #stop()
-
-plotlmrdia(lmrdia(), xlim=c(0.1,0.35), ylim=c(0.1,0.20), empty=TRUE, autoaxes=FALSE,
-           xaxs="i", yaxs="i", lwd.cex=1.3)
-par(las=1)
-xtix <- c(0.07, seq(0.10, 0.35, by=0.05) )
-ytix <- c(0.11, seq(0.12, 0.20, by=0.02) )
-axis(1, at=xtix, labels=sprintf("%0.2f", xtix), lwd=0, lwd.ticks=1)
-axis(3, at=xtix, labels=FALSE, lwd=0, lwd.ticks=1)
-axis(2, at=ytix, labels=sprintf("%0.2f", ytix), lwd=0, lwd.ticks=1)
-axis(4, at=ytix, labels=FALSE, lwd=0, lwd.ticks=1)
-xtix <- c(      seq(0.11, 0.34, by=0.01) )
-ytix <- c(      seq(0.11, 0.19, by=0.01) )
-xtix <- as.numeric( xtix[grep("\\d[05]$", sprintf("%0.2f", xtix), invert=TRUE)] )
-axis(1, at=xtix, labels=FALSE, lwd=0, lwd.ticks=1, tcl=-0.3)
-axis(3, at=xtix, labels=FALSE, lwd=0, lwd.ticks=1, tcl=-0.3)
-axis(2, at=ytix, labels=FALSE, lwd=0, lwd.ticks=1, tcl=-0.3)
-axis(4, at=ytix, labels=FALSE, lwd=0, lwd.ticks=1, tcl=-0.3)
-
-points(Z$tau3,      Z$tau4,      pch=21, cex=log10(Z$nsim)-3.2, col="turquoise4", bg="turquoise")
-plotlmrdia(lmrdia(usrtrim=TRUE), add=TRUE, nopoints=TRUE, autolegend=TRUE, xleg="topleft",
-           noaep4=TRUE, nogev=TRUE, nogpa=TRUE, nogov=TRUE, noglo=TRUE, nopdq3=TRUE,
-           nolimits=TRUE, lwd.cex=2, expand.names=TRUE, legendcex=0.7)
-nevels <- seq(3,14, by=1)
-for(i in seq_len(length(nevels))) {
-  wnt <- nevels[i] == Z$n
-  #with(Z[wnt,], points(weighted.mean(tau3, nsim), weighted.mean(tau4, nsim), pch=16, cex=0.5))
-  #with(Z[wnt,], text(  weighted.mean(tau3, nsim), weighted.mean(tau4, nsim), nevels[i],
-  #                   pos=1, offset=0.3, cex=0.7, col="grey10", font=2))
-}
 
 
 usepade <- FALSE
